fdadc6bdbe20fcdee1dcb6aa7c20bc5aac00aed2
Kevin Martensson <kevinmartensson@gmail.com>
2014-05-06 10:34:54 +0200

    Fetch list from apache.org
    Fixes #1.

--- test.js
+++ test.js
@@ -6,7 +6,10 @@ var extList = require('./');
 
 describe('extList()', function () {
     it('should return a list of known file types', function (cb) {
-        assert(Object.keys(extList).length > 0);
-        cb();
+        extList(function (err, obj) {
+            assert(!err);
+            assert(Object.keys(obj).length > 50);
+            cb();
+        });
     });
 });
