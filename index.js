5ba5c0e7a8abba8bb0c9e9b4a8229f143538d533
Kevin Martensson <kevinmartensson@gmail.com>
2017-05-24 11:35:11 +0200

    Use `mime-db` instead of manually fetching the list

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 'use strict';
-module.exports = () => require('./ext-list.json');
+const mimeDb = require('mime-db');
+
+module.exports = () => {
+	const ret = {};
+
+	for (const x of Object.keys(mimeDb)) {
+		const val = mimeDb[x];
+
+		if (val.extensions && val.extensions.length > 0) {
+			for (const y of val.extensions) {
+				ret[y] = x;
+			}
+		}
+	}
+
+	return ret;
+};
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import m from './';
+import m from '.';
 
 test(t => {
 	t.truthy(Object.keys(m()).length);
